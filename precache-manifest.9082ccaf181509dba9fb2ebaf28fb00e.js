8e86411956a8e8c258590a10b08681d4e5eafcfa
Tian Zheng <zhengtian07@outlook.com>
2019-07-08 09:45:45 -0400

    Updates

---
+++ precache-manifest.9082ccaf181509dba9fb2ebaf28fb00e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da7f7bac26979c27cfe86f2b9fbfdbd",
+    "url": "/react_app/index.html"
+  },
+  {
+    "revision": "b6fbe6f11d6ae101b0a1",
+    "url": "/react_app/static/css/main.856419da.chunk.css"
+  },
+  {
+    "revision": "ac74bf980c7e7f454524",
+    "url": "/react_app/static/js/2.30949a74.chunk.js"
+  },
+  {
+    "revision": "b6fbe6f11d6ae101b0a1",
+    "url": "/react_app/static/js/main.31d559fc.chunk.js"
+  },
+  {
+    "revision": "dd7a70cdeda87e112e79",
+    "url": "/react_app/static/js/runtime~main.cba007e3.js"
+  }
+]);
